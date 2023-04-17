b90889e35817560d53074b7e2cf6b99e7140c6d7
shima1364 <sh.movafagh.474@gmail.com>
2023-04-17 11:22:51 +0200

    15

--- Booking/src/pages/Reservation.jsx
+++ Booking/src/pages/Reservation.jsx
@@ -1,24 +1,39 @@
-import jwt from'jsonwebtoken';
-import React from "react";
+import jwt from 'jsonwebtoken';
+import React, { useEffect } from "react";
 import NavbarApp from "../layouts/Navbar";
 import axios from "axios";
 import { useContext, useState } from "react";
 import { DataContext } from "../context/dataContext";
 
-function Reservation () {
-const ctx = useContext(DataContext);
-const HotelId = ctx.reservedHotel;
-const token = ctx.token;
-decoded_token = jwt.decode(token, verify=False)
-user_id = decoded_token['userId']
-(console.log(user_id))
-useEffect(() => {
-            axios  .post (`http://localhost:8800/api/users/${user_id}/reservations`, {  });
- 
-  }, [id])
-return
-(<>
-</>)
- }
+function Reservation() {
+  const ctx = useContext(DataContext);
+  const HotelId = ctx.reservedHotel;
+  const token = ctx.token;
+  const decoded_token = jwt.decode(token, verify=false);
+  const user_id = decoded_token['userId'];
 
- export default Reservation
+  useEffect(() => {
+    axios.post(`http://localhost:8800/api/users/${user_id}/reservations`, {
+      user: {user_id},
+      hotel: {HotelId},
+      checkInDate: ctx.StartDate,
+      checkOutDate: ctx.FinalDate,
+      numberOfRooms: ctx.RoomCounter,
+    }).then(response => {
+      console.log(response);
+      // handle response from API
+    }).catch(error => {
+      console.log(error);
+      // handle error
+    });
+  }, [HotelId, ctx.StartDate, ctx.FinalDate, ctx.RoomCounter]);
+
+  return (
+    <>
+      <NavbarApp />
+      {/* your JSX code here */}
+    </>
+  );
+}
+
+export default Reservation;
