d0687ec7b6570cd946d4cd3b05d7eccf913d8c61
shima1364 <sh.movafagh.474@gmail.com>
2023-04-19 20:28:35 +0200

    21

--- Booking/src/pages/Reservation.jsx
+++ Booking/src/pages/Reservation.jsx
@@ -1,19 +1,14 @@
-import React from "react";
-
-import { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import NavbarApp from "../layouts/Navbar";
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 
-
-
 const ReservationHistory = () => {
-    const userId = sessionStorage.getItem("userId");
-    console.log(userId);
+  const userId = sessionStorage.getItem("userId");
   const [userHistory, setUserHistiry]= useState([]);
 
   useEffect(() => {
-
     const getEntryById = async () => {
       try {
         const response = await fetch(
@@ -27,27 +22,36 @@ const ReservationHistory = () => {
     };
     getEntryById();
   }, [userId]);
-  console.log(userHistory);
 
- 
+  const formatDate = (date) => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(date).toLocaleDateString(undefined, options);
+  };
 
   return (
-    <div>
+    <div className="container">
       <NavbarApp />
-      {userHistory.map((items) => (
-      <Card style={{ width: '18rem' }} key={items.id}>
-      <Card.Body>
-        <Card.Title>{items.hotelName}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">{items.numberOfRooms}</Card.Subtitle>
-        <Card.Text>
-          your resevation from {items.checkInDate} to {items.checkOutDate}  was complited
-        </Card.Text>
-        <Link to={`/hotels/${items.hotel}`}>Card Link</Link>
-      </Card.Body>
-    </Card>))}
-    
+      <div className="row mt-3">
+        {userHistory.map((items) => (
+          <div className="col-md-6 mb-4" key={items.id}>
+            <Card>
+              <Card.Body>
+                <Card.Title>{items.hotelName}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{items.numberOfRooms} rooms</Card.Subtitle>
+                <Card.Text>
+                  Your reservation from {formatDate(items.checkInDate)} to {formatDate(items.checkOutDate)} was completed.
+                </Card.Text>
+                <Link to={`/hotels/${items.hotel}`}>
+                  <Button variant="primary">View hotel details</Button>
+                </Link>
+              </Card.Body>
+            </Card>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default ReservationHistory;
+
